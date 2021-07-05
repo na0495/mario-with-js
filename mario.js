cc9fee138293c900e6d3b3ed4c3c38e0645d8a77
na4095 <saadmrabet008@gmail.com>
2021-07-05 19:24:15 +0100

    update the camera movement the monster kills et lose screen

--- mario.js
+++ mario.js
@@ -27,7 +27,17 @@ loadSprite('blueEvilShroom', 'SvV4ueD.png')
 loadSprite('blueSurprise', 'RMqCc1G.png')
 
 
-scene("game", () => {
+//game setting 
+
+const MoveSpeed = 120
+const JumpForce = 360
+const BigJumpForce = 500
+const fallDeath = 400
+const enemySpeed = -20
+let isJumping = true
+let CurrentJumpForce = JumpForce
+
+scene("game", ({ score }) => {
     layers(['bg', 'obj', 'ui'], 'obj')
 
     const gameBoard = [
@@ -61,8 +71,8 @@ scene("game", () => {
         height: 20,
         '=': [sprite('block'), solid()],
         '$': [sprite('coin'), 'coin'],
-        '%': [sprite('surprise'), solid(), 'coin-surprise'],
-        '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+        '%': [sprite('surprise'), solid(), 'coinSurprise'],
+        '*': [sprite('surprise'), solid(), 'mushroomSurprise'],
         '}': [sprite('unboxed'), solid()],
         '(': [sprite('pipeBottomLeft'), solid(), scale(0.5)],
         ')': [sprite('pipeBottomRight'), solid(), scale(0.5)],
@@ -72,25 +82,104 @@ scene("game", () => {
         '#': [sprite('mushroom'), solid(), 'mushroom', body()],
         '!': [sprite('blueBlock'), solid(), scale(0.5)],
         '£': [sprite('blueBrick'), solid(), scale(0.5)],
-        'z': [sprite('blueEvilShroom'), solid(), scale(0.5), 'dangerous'],
+        'z': [sprite('blueEvilShroom'), solid(), scale(0.5), 'danger'],
         '@': [sprite('blueSurprise'), solid(), scale(0.5), 'coin-surprise'],
         'x': [sprite('blueSteel'), solid(), scale(0.5)],
 
     }
 
     const gameLevel = addLevel(gameBoard, levelConfig)
+    
+    const scoreLabel = add([text(score), pos(30, 6), layer('ui'), { value: 'score', }])
 
-    const scoreLabel = add([text('score'), pos(30, 6), layer('ui'), { value: 'score', }])
+    add([text('level ' + parseInt(level + 1) ), pos(40, 6)])
 
-    add([text('level' + 'test' , pos(4,6))])
+    function big() {
+        let timer = 0
+        let isBig = false
+        return {
+            update() {
+                if (isBig) {
+                    timer -=dt()
+                    if (timer <= 0) {
+                        this.smallify
+                    }
+                }
+            },
+            isBig() {
+                return isBig
+            },
+            smallify() {
+                CurrentJumpForce = JumpForce
+                this.scale = vec2(1)
+                timer = 0 
+                isBig = false
+            },
+            biggify(time) {
+                CurrentJumpForce = BigJumpForce
+                this.scale = vec2(2)
+                timer = time
+                isBig = false
+            }
+        }
+    }
 
     const player = add([
-        sprite('mario'), solid(), pos(30, 0), body(), origin('bot')
+        sprite('mario'), solid(), pos(30, 0), body(), origin('bot'), big()
     ])
 
-    const MoveSpeed = 120
-    const JumpForce = 360
-    let isJumping = true
+    action('mushroom', (m) => {
+        m.move(20, 0)
+    })
+
+    action('danger', (d) => {
+        d.move(enemySpeed, 0)  
+    })
+
+    player.on("headbump", (obj) => {
+        if (obj.is('coinSurprise')) {
+            gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+        }
+
+        if (obj.is('mushroomSurprise')) {
+            gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+        }
+    })
+
+    player.collides('mushroom', (m) =>  {
+        destroy(m)
+        player.biggify(6)
+    })
+
+    player.collides('coin', (c) => {
+        destroy(c)
+        scoreLabel.value++
+        scoreLabel.text = scoreLabel.value
+    })
+    // killing ennemys and losing 
+
+    player.collides('danger', (d) => {
+        if (isJumping) {
+          destroy(d)
+        } else {
+          go('lose', { score: scoreLabel.value})
+        }
+    })
+
+    player.action(() => {
+        camPos(player.pos)
+        if (player.pos >= fallDeath) {
+            globalThis('lose', {score :scoreLabel.value})
+        }
+    })
+
+    
+
+    //? Moving setting
 
     keyDown('left', () => {
         player.move(-MoveSpeed, 0)
@@ -108,10 +197,14 @@ scene("game", () => {
     keyPress('space', () => {
         if (player.grounded()) {
           isJumping = true
-          player.jump(JumpForce)
+          player.jump(CurrentJumpForce)
         }
     })
 
 })
 
-start("game")
+scene('lose', ({ score }) => {
+    add([text(score, 32), origin('center'), pos(width()/2, height()/ 2)])
+  })
+
+  start("game", { level: 0, score: 0})
